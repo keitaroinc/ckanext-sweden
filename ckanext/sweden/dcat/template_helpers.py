ce0a190600798a83a93f2f4d87ef6e0b9f4b6e8f
Petar Efnushev <petar.efnushev@gmail.com>
2016-06-10 11:26:46 +0200

    Add custom field translation template helper

--- ckanext/sweden/dcat/template_helpers.py
+++ ckanext/sweden/dcat/template_helpers.py
@@ -1,8 +1,31 @@
 import json
-
+import os
 
 def json_loads(string):
     try:
         return json.loads(string)
     except ValueError:
         return None
+
+
+def get_localized_value(string, locale='en'):
+    path = os.path.join(os.path.dirname(__file__),
+                                       'translations',
+                                       'dcat_ap_choices.json')
+    
+    with open(path) as f:
+        
+        translations = json.load(f)
+        
+        if string.startswith('[') and string.endswith(']'):
+            return '[{}]'.format(','.join(translations.get(v, v).get(locale, v) \
+                                           for v in eval(string)))
+        
+        _ = translations.get(string, None)
+        if _ is None:
+            return string
+        
+        return _.get(locale)
+    
+    
+    
